ec7336c9cd02987190913004f2350418a983cc10
Egor <des2107@mail.ru>
2023-09-20 23:23:18 +0300

    это сервер тоже

---
+++ server/web/handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
+	// функция http.NotFound() для возвращения клиенту ошибки 404.
+	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
+	// продолжит работу и выведет сообщение "Привет из SnippetBox" как ни в чем не бывало.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// файл home.page.tmpl должен быть *первым* файлом в срезе.
+	files := []string{
+		"./ui/html/home.html",
+	}
+
+	// Используем функцию template.ParseFiles() для чтения файлов шаблона.
+	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// используя функцию http.Error() мы отправим пользователю
+	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
---
+++ server/web/web-server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
+	flag.Parse()
+
+	// Используйте log.New() для создания логгера для записи информационных сообщений. Для этого нужно
+	// три параметра: место назначения для записи логов (os.Stdout), строка
+	// с префиксом сообщения (INFO или ERROR) и флаги, указывающие, какая
+	// дополнительная информация будет добавлена. Обратите внимание, что флаги
+	// соединяются с помощью оператора OR |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Создаем логгер для записи сообщений об ошибках таким же образом, но используем stderr как
+	// место для записи и используем флаг log.Lshortfile для включения в лог
+	// названия файла и номера строки где обнаружилась ошибка.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Применяем созданные логгеры к нашему приложению.
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
+}
