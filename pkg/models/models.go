886611053b83580260fd20c5629554d51e91c43a
Egor <des2107@mail.ru>
2023-09-24 11:20:07 +0300

    модели для базы

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -8,11 +8,21 @@ import (
 var ErrNoRecord = errors.New("models: подходящей записи не найдено")
 
 type trunk struct {
-	ID      int
-	Title   string
-	Content string
-	Created time.Time
-	Expires time.Time
+	ID                  int
+	Model               string
+	ParamCharge         int
+	ParamQn             float64
+	ParamQg             float64
+	ParamQv             float64
+	P                   float64
+	T                   float64
+	ParamFlowRegime     string
+	ParamFacticVelocity int
+	ParamCriticVelocity int
+	ParamCrash          int
+	ParamLifetime       int
+	ResidualResource    int
+	Created             time.Time
 }
 
 /*
