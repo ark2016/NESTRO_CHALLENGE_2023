cfbb37ec805b30150c89a308ba10112d70b674a2
Лебедев Аркадий <lebedevai2@student.bmstu.ru>
2023-09-26 01:57:30 +0300

    add: заполнение JSON

--- client/add_data.py
+++ client/add_data.py
@@ -1,6 +1,8 @@
 from backend_mat_model.data import *
 from backend_mat_model.database import *
 import pandas as pd
+import numpy as np
+import json
 
 df = pd.read_csv("Приложение 2. Данные о потоке нефти, газа и воды по каждому трубопроводу.xlsx - Данные для кейса.csv")
 df = df.fillna(0)
@@ -26,7 +28,15 @@ for i in range(len(names)):
     param_Qn = [Q_n.iloc[i, k] for k in range(len(Q_n.iloc[i]))]
     param_Qg = [Q_g.iloc[i, k] for k in range(len(Q_g.iloc[i]))]
     param_Qv = [V_l.iloc[i, k] for k in range(len(V_l.iloc[i]))]
-
+    param_charge = None
+    param_P = average_pressure_in_area(P_start[i], P_end[i])
+    param_T = np.mean(T_cp_n, P_cp_k)
+    param_flow_regime = None
+    param_critic_velocity_param_factic_velocity = None
+    transition = None
+    param_critic_velocity = None
+    param_crash = None
+    param_lifetime = internal_pressure_pipeline_element_can_withstand([thickness], )
     dictionary = {
         "name": name,
         "param_charge": param_charge,
@@ -42,4 +52,8 @@ for i in range(len(names)):
         "param_critic_velocity": param_critic_velocity,
         "param_lifetime": param_lifetime
     }
+    json_object = json.dumps(dictionary, indent=4)
+    with open("example.json", "w") as outfile:
+        outfile.write(json_object)
+
 # print(len(P_cp_k.iloc[0]))
